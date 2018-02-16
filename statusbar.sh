6d13cf4b663317b936fd0f363d0d025e6a5ab94e
Peder Stray <peder@inne.proxdynamics.com>
2018-02-16 15:54:01 +0100

    Use new functions

--- statusbar.sh
+++ statusbar.sh
@@ -8,7 +8,7 @@ _status_set() {
     [ -z "$_status_line" ] && _status_init
     _status_data="$(echo -ne "\e[s\e[$_status_line;1H\e[1;44;36m[\e[37m$(date +%H:%M)\e[36m][\e[37mstatus...\e[36m]\e[K\e[u")"
 }
-_add_prompt_cmd _status_set 99
+:prompt:cmd _status_set 99
 
 _winch_hook() {
     local lines=$(tput lines)
